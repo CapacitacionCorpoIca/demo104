6075eecb455ac139ad494d37deca8b603e5b275c
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2015-12-03 08:07:18 -0500

    Moment and Datepicker

--- www/app/todo/create-todo.controller.js
+++ www/app/todo/create-todo.controller.js
@@ -10,20 +10,39 @@ Controlador para la lista de tareas
 
     CreateTodoController.$inject = [
     	'todoService',
-    	'$state'
+    	'$state',
+      '$cordovaDatePicker'
     ];
 
-    function CreateTodoController( todoService, $state ){
+    function CreateTodoController( todoService, $state, $cordovaDatePicker ){
       var vm = this;
       //Atributes
       vm.newTodo = {};
       //Accions
       vm.create = create;
+      vm.showDate = showDate;
 
       function create(){
       	todoService.create( vm.newTodo );
       	vm.newTodo = {};
       	$state.go('todos');
+      }
+
+      function showDate(){
+        var options = {
+          date: new Date(),
+          mode: 'date', // or 'time'
+          maxDate: moment().subtract(18, 'years')._d.getTime(),
+          doneButtonLabel: 'Aceptar',
+          doneButtonColor: '#F2F3F4',
+          cancelButtonLabel: 'Cancelar',
+          cancelButtonColor: '#000000'
+        };
+
+        $cordovaDatePicker.show(options)
+        .then(function(date){
+          alert(date);
+        });
       }
 
     }
